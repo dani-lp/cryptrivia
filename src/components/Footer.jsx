9a99060d15b3986c131d1461c7f9ea84fb863c2a
Dani Lauzurica <danilau11@gmail.com>
2023-05-20 20:23:08 +0200

    new Logo, footer MIT notice

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -86,8 +86,7 @@ function SmallPrint() {
   return (
     <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
-        &copy; Copyright {new Date().getFullYear()}. Todos los derechos
-        reservados.
+        Licencia MIT, {new Date().getFullYear()}.
       </p>
       <div className="flex gap-4">
         <SocialLink
